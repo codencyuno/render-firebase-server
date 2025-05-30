26d2a57eaa00a4abe548e57794f0376430caf2d4
Codency Uno <codencyuno@gmail.com>
2025-05-30 06:06:29 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const app = require("./app");
 const { Server } = require("socket.io");
+const authRoutes = require("./routes/auth");
 const { setupSockets } = require("./sockets");
 require("dotenv").config();
 
