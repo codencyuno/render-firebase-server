1491701910dc9fec1b1ca44f6d25e9b71e496278
Codency Uno <codencyuno@gmail.com>
2025-05-30 06:13:31 +0100

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -7,4 +7,5 @@ admin.initializeApp({
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
     privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
   }),
+  databaseURL: process.env.FIREBASE_DATABASE_URL, // ✅ ADD THIS
 });
