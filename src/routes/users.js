7463b15759b30b6ecf1532d768be098afcbf0f1a
Codency Uno <codencyuno@gmail.com>
2025-05-30 17:21:37 +0100

    Update users.js

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const db = require("firebase-admin").database();
+const db = require("../firebase").database();
 const { authMiddleware } = require("../middlewares/authMiddleware");
 
 router.get("/users/me", authMiddleware, async (req, res) => {
