44fa4784c6127af8d0d8e54ffc1e0de069c1d8cb
Codency Uno <codencyuno@gmail.com>
2025-05-30 17:11:17 +0100

    Update authMiddleware.js

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,17 +1,17 @@
 const jwt = require("jsonwebtoken");
 
-module.exports = (req, res, next) => {
-  const authHeader = req.headers.authorization;
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    return res.status(401).send("Unauthorized");
-  }
+function authMiddleware(req, res, next) {
+  const token = req.headers.authorization?.split(" ")[1];
 
-  const token = authHeader.split(" ")[1];
+  if (!token) return res.status(401).send("No token provided");
+
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
     next();
-  } catch (err) {
-    res.status(401).send("Invalid or expired token");
+  } catch {
+    return res.status(401).send("Invalid token");
   }
-};
+}
+
+module.exports = { authMiddleware };
